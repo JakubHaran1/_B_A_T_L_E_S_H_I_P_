bcd50aacd13e9cab8056d8e3c94d7b0064552991
Jakub Harań <jakub.haran02@gmail.com>
2024-02-10 05:24:49 +0100

    add ships to get_board

--- batleship.py
+++ batleship.py
@@ -3,7 +3,7 @@ import os
 
 class Player:
     row_key = [" A "," B "," C "," D "," E " ," F "," G "," H "," I "," J "]
-    column_key = [" 1"," 2"," 3"," 4"," 5"," 6"," 7"," 8"," 9"," 10"]
+    column_key = ["1 ","2 ","3 ","4 ","5 ","6 ","7 ","8 ","9 ","10 "]
     banned_symbols = ["<",".",">","?","/",";",":",'""',"[","{","]","}","\\","+","=","-","_",")","(","*","&","^","%","$","#","@","!","`","~","''"]
     
     def __init__(self, name):
@@ -20,7 +20,7 @@ class Player:
         
         # Wypełnianie tablicy
         for i in range(10): #liczba kolumn i wierszy
-            board[self.row_key[i]] = ["|0|" * 10]
+            board[self.row_key[i]] = ["|0|"] * 10
             
         return board
 
@@ -28,8 +28,12 @@ class Player:
     def print_board(self,board):
         # Wyświetlanie zmiennych kolumnowych
         print("   ",*self.column_key)
-        for i in range(10): #liczba kolumn i wierszy
-            print(self.row_key[i],*board[self.row_key[i]])
+        for row in self.row_key: 
+            print(row, end="")
+            for el in board[row]:
+                print(el,end="")
+            print()
+            
         print()
     
     #funkcja określająca typ statku na podstawie aktualnej długości głównej tablicy statkowej
@@ -57,7 +61,6 @@ class Player:
         index_el = ""
         first_zone = ""
         second_zone = ""
-        orientation = "column" #do wywalenia, zrobione do szybszego sprawdzania self_protector dla kolumn
 
         # Wyliczanie zabezpieczonych pól:
         if orientation == "row":
@@ -82,13 +85,13 @@ class Player:
                     
             # Wyliczanie "dodatkowych" bocznych pól
             # Pole lewe skrajne
-            index_el = self.column_key.index(f" {min_value[1:]}")
+            index_el = self.column_key.index(f"{min_value[1:]} ")
             if index_el != 0:
                 self.add_protect_field(min_value, min_value[:1], self.column_key[index_el - 1].strip())
                 
             
             # Pole prawe skrajne
-            index_el = self.column_key.index(f" {max_value[1:]}")
+            index_el = self.column_key.index(f"{max_value[1:]} ")
             if index_el != len(self.column_key) - 1:
                 self.add_protect_field(max_value, max_value[:1], self.column_key[index_el + 1].strip())
         else:  
@@ -99,7 +102,7 @@ class Player:
                 else: max_value = el
 
                 # Zmienna pomocnicza do wyliczenia lewego i prawego bezpiecznego pola 
-                index_el = self.column_key.index(f" {el[1:]}")
+                index_el = self.column_key.index(f"{el[1:]} ")
                 
                 # wyliczenie lewego zabezpieczonego pola
                 if index_el != 0:
@@ -119,11 +122,21 @@ class Player:
             index_el = self.row_key.index(f" {max_value[:1]} ")
             if index_el != len(self.row_key) - 1:
                 self.add_protect_field(max_value, self.row_key[index_el + 1].strip(), max_value[1:])
-            
+
+    def draw_ship(self,ship,board_get):
+        arr =""
+        for el in ship:
+            key = el[:1]
+            value = int(el[1:])
+            arr = board_get[f" {key} "]
+            arr[value-1] = "🚢 "
+
+                    
             
 
 class Ai(Player):
-    pass            
+    def __init__(self,name):
+        self.name = name           
             
 
     
@@ -135,106 +148,126 @@ class Ai(Player):
 
 class Batleship:
     def __init__(self):
-        players = 0
-        self.player1 = Player("Player")
-        self.player2 = Player("AI")
+        self.players = self.create_player()
         self.pre_game()
         self.start_game()
         
+    # Tworzenie graczy
+    def create_player(self):
+        print("---GRA W STATKI---")
+        players = input("Wybierz typ gry:\n 1 - gra z drugim graczem,\n 0 lub dowolny inny symbol - gra z komputerem \n\nTwój wybór:")
+        if players == "1":
+            players = []
+            for i in range(2):
+                name = input(f"Podaj imię gracza nr. {i+1}: ")
+                players.append(Player(name))
+        else:
+            players = []
+            name = input(f"Podaj imię gracza nr. 1: ")
+            players.append(Player(name))
+            players.append(Ai("Ai")) 
+        
+        return(players)
+
+
     
     # Instrukcja gry 
     def pre_game(self):
         os.system('cls')
-        print("GRA W STATKI",end='\n\n')
+        print("---GRA W STATKI---",end='\n\n')
         print("Zasady:")
         print("Masz do wyboru: cztery jednomasztowce, trzy dwumasztowce, dwa trzymasztowce i jeden czteromasztowiec")
         print("Musisz rozmieszczać pojedyncze statki w obrębie jednego wiersza lub kolumny tzn. nie wolno ci postawić np.dwumasztowca na [A1,B2]")
         print("Statki nie mogą się stykać")
         print("Posiadasz dwie plansze:",end='\n\n')
         print("-strzelnicza na której będziesz zaznaczać swoje strzały")
-        self.player1.print_board(self.player1.board_shot)
+        self.players[0].print_board(self.players[0].board_shot)
         print("-celownicza na której będą zaznaczane strzały i trafienia AI")
-        self.player1.print_board(self.player1.board_get)
+        self.players[0].print_board(self.players[0].board_get)
         accept = input("czy akceptujesz zasady gry ? 1 - TAK; 0 - NIE: ")
         os.system("cls") 
 
 
 
     def start_game(self):
-        orientation = "row"
-        while len(self.player1.ships) != 10:
-            size = self.player1.type_ship(len(self.player1.ships))
-            print("To jest twoja tablica ze statkami:")
-            self.player1.print_board(self.player1.board_get)
-            ship = input(f"Podaj umiejscowienie statku o długości {size}: ")
-            ship = ship.upper()
-            os.system('cls')
-            
-            try:
-                if ship == "": raise Exception("Uwaga! Nie podano pozycji statku!")
-
-                # Sprawdzannie czy znajdują się niedozwolone symbole
-                if not any(symbol.strip() in ship for symbol in self.player1.banned_symbols):
-                    # Dzielenie statku na poszczególne "frakcje"
-                    ship = ship.split(",")
-                else:
-                    raise Exception("W pozycji znajdują się niedozwolone symbole!!")
-
-                # Sprawdzanie długości statku
-                if len(ship) != size: raise Exception("Niepoprawna długość statku!!")
-
-                # Sprawdzanie czy wszystkie klucze są unikalne i nie znajduja się w safety_zone ani ships
-                for el in ship:
-                    if self.player1.ships.count(el) > 1: raise Exception(f"Wartość {el} występuje w deklaracji statku więcej niż jeden raz")
-
-                    el = el.replace(" ","")
-                    if f" {el[:1]} " not in self.player1.row_key:
-                        raise Exception(f"Niepoprawna wartość na pozycji wierszowej - {el[:1]} w {el}! Mają to być litery od A-J")
+        for player in self.players:
+            if player.name != "Ai":
+                while len(player.ships) != 10:
+                    orientation = "row"
+                    size = player.type_ship(len(player.ships))
+                    print("To jest twoja tablica ze statkami:")
+                    player.print_board(player.board_get)
+                    ship = input(f"Podaj umiejscowienie statku o długości {size}: ")
+                    ship = ship.upper()
+                    os.system('cls')
                     
-                    if f" {el[1:]}" not in self.player1.column_key:
-                        raise Exception(f"Niepoprawna wartość na pozycji kolumnowej - {el[1:]} w {el}! Mają to być cyfry od 1-10")
+                    try:
+                        if ship == "": raise Exception("Uwaga! Nie podano pozycji statku!")
 
-                    if el in self.player1.protect_zone:
-                        raise Exception(f"Pozycja {el} jest pozycją chronioną innego statku!")
-                    
-                    for boat in self.player1.ships: 
-                        if el in boat:
-                            raise Exception(f"Na pozycji {el} jest już umiejscowiony statek!")
+                        # Sprawdzannie czy znajdują się niedozwolone symbole
+                        if not any(symbol.strip() in ship for symbol in player.banned_symbols):
+                            # Dzielenie statku na poszczególne "frakcje"
+                            ship = ship.split(",")
+                        else:
+                            raise Exception("W pozycji znajdują się niedozwolone symbole!!")
 
+                        # Sprawdzanie długości statku
+                        if len(ship) != size: raise Exception("Niepoprawna długość statku!!")
 
-                # Ustalenie pozycji statku
-                if len(ship) > 1:
-                    # Przypisywanie wiersza do zmiennej pomocniczej - row
-                    row = ship[0][:1] # Litera pierwszej części statku 
-                    
-                    # Przypisywanie kolumny do zmiennej pomocniczej - column
-                    column = ship[0][1:] # Cyfra pierwszej części statku
-                    
-                    # Sprawdzanie w jakiej orientacji jest statek - na bazie drugiej części statku
-                    if ship[1][:1] == row:
-                        orientation = "row"
-                    elif ship[1][1:] == column:
-                        orientation = "column"
-                    else: raise Exception("Statek musi znajdywać się w pozycji poziomej lub pionowej (te same kolumny lub wiersze) !!")
-
-                    # Sprawdzanie czy pozycja jest utrzymywana
-                    if orientation == "row": 
+                        # Sprawdzanie czy wszystkie klucze są unikalne i nie znajduja się w safety_zone ani ships
                         for el in ship:
-                            if el[:1] != row: raise Exception("Statek nie jest umiejscowiony poziomo!!")
-                    else:
-                        for el in ship:
-                            if el[1:] != column: raise Exception("Statek nie jest umiejscowiony pionowo!!")
-                
-                self.player1.ships.append(ship)
-                
-            except Exception as e:
-                print(f"Uwaga! Błąd w deklaracji statku: {e}")
-                continue
+                            if player.ships.count(el) > 1: raise Exception(f"Wartość {el} występuje w deklaracji statku więcej niż jeden raz")
 
-            
+                            el = el.replace(" ","")
+                            if f" {el[:1]} " not in player.row_key:
+                                raise Exception(f"Niepoprawna wartość na pozycji wierszowej - {el[:1]} w {el}! Mają to być litery od A-J")
+                            
+                            if f"{el[1:]} " not in player.column_key:
+                                raise Exception(f"Niepoprawna wartość na pozycji kolumnowej - {el[1:]} w {el}! Mają to być cyfry od 1-10")
 
-            self.player1.field_protector(orientation,ship)
-            
+                            if el in player.protect_zone:
+                                raise Exception(f"Pozycja {el} jest pozycją chronioną innego statku!")
+                            
+                            for boat in player.ships: 
+                                if el in boat:
+                                    raise Exception(f"Na pozycji {el} jest już umiejscowiony statek!")
+
+
+                        # Ustalenie pozycji statku
+                        if len(ship) > 1:
+                            # Przypisywanie wiersza do zmiennej pomocniczej - row
+                            row = ship[0][:1] # Litera pierwszej części statku 
+                            
+                            # Przypisywanie kolumny do zmiennej pomocniczej - column
+                            column = ship[0][1:] # Cyfra pierwszej części statku
+                            
+                            # Sprawdzanie w jakiej orientacji jest statek - na bazie drugiej części statku
+                            if ship[1][:1] == row:
+                                orientation = "row"
+                            elif ship[1][1:] == column:
+                                orientation = "column"
+                            else: raise Exception("Statek musi znajdywać się w pozycji poziomej lub pionowej (te same kolumny lub wiersze) !!")
+
+                            # Sprawdzanie czy pozycja jest utrzymywana
+                            if orientation == "row": 
+                                for el in ship:
+                                    if el[:1] != row: raise Exception("Statek nie jest umiejscowiony poziomo!!")
+                            else:
+                                for el in ship:
+                                    if el[1:] != column: raise Exception("Statek nie jest umiejscowiony pionowo!!")
+                        
+                        player.ships.append(ship)
+                        
+                    except Exception as e:
+                        print(f"Uwaga! Błąd w deklaracji statku: {e}")
+                        continue
+
+                    
+
+                    player.field_protector(orientation,ship)
+                    player.draw_ship(ship,player.board_get)
+            else:
+                print(player.name)
             
             
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-# Wyliczanie dolnego pola
-                # if index_el != len(self.row_key) - 1:
-                #     self.add_protect_field(second_zone, self.row_key[index_el + 1 ], el[1:])
+el
